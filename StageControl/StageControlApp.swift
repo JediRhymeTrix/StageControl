757860828c6d4e7dc92299ab955d6fe750d1ff90
Vedant Vohra <ved914980@gmail.com>
2023-03-15 07:16:18 -0500

    Initial Commit

---
+++ StageControl/StageControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  StageControlApp.swift
+//  StageControl
+//
+//  Created by Vedant Vohra on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct StageControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
